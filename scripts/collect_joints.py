40ef50eb9a30de7160166198963962ac76f6e447
MacLeek <liyang@happylyang.com>
2016-01-08 20:57:42 +0800

    fix some joints computing errors

--- scripts/collect_joints.py
+++ scripts/collect_joints.py
@@ -29,8 +29,8 @@ class Tracker():
 
     def __init__(self):
         rospy.init_node('tracker_mykinect', anonymous=True)
-    	self.myFrame = '/nao'
-    	self.pub_trans = rospy.Publisher(self.myFrame, jointdatalist, queue_size=10)
+        self.myFrame = '/nao'
+        self.pub_trans = rospy.Publisher(self.myFrame, jointdatalist, queue_size=10)
         rate = rospy.get_param('~rate', 3)
 
         r = rospy.Rate(rate)
@@ -40,13 +40,13 @@ class Tracker():
         rospy.sleep(5.0)
         rospy.loginfo("Tracking started!")
         while not rospy.is_shutdown():
-	    trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'head_1', rospy.Duration())
-	    head = np.array([trans[0], trans[1], trans[2]])
+        trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'head_1', rospy.Duration())
+        head = np.array([trans[0], trans[1], trans[2]])
 
-	    trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'neck_1', rospy.Duration())
+        trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'neck_1', rospy.Duration())
             neck = np.array([trans[0], trans[1], trans[2]])
 
-    	    trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'right_shoulder_1', rospy.Duration())
+            trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'right_shoulder_1', rospy.Duration())
             
             right_shoulder = np.array([trans[0], trans[1], trans[2]])
 
@@ -64,91 +64,126 @@ class Tracker():
 
             trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'left_hand_1', rospy.Duration())
             left_hand = np.array([trans[0], trans[1], trans[2]])
-		
-	    # left shoulder roll/pitch
+    
+        trans, rot = self.tf.lookupTransform('/openni_depth_frame', 'torso_1', rospy.Duration())
+            torso = np.array([trans[0], trans[1], trans[2]])
+        
+        # left shoulder roll/pitch
             left_shoulder_elbow = left_elbow - left_shoulder
             left_shoulder_neck = right_shoulder - left_shoulder
 
             left_shoulder_elbow = left_shoulder_elbow / np.linalg.norm(left_shoulder_elbow)
             left_shoulder_neck = left_shoulder_neck / np.linalg.norm(left_shoulder_neck)
 
-            left_shoulder_angle_roll = math.acos(left_shoulder_elbow.dot(left_shoulder_neck)) - math.pi/2
+            left_shoulder_angle_roll = math.acos(left_shoulder_elbow.dot(left_shoulder_neck)) - math.pi/2   
 
-            left_shoulder_angle_pitch = -1.0 * math.asin(left_shoulder_elbow[2])
+        # neck to torso vector
+            neck_torso_vec = torso - neck
+            left_torso_vertical_vec = np.cross(neck_torso_vec, left_shoulder_neck)
+            left_torso_vertical_vec = left_torso_vertical_vec / np.linalg.norm(left_torso_vertical_vec)
+            left_shoulder_vertical_vec = np.cross(left_shoulder_neck, left_shoulder_elbow)
+            left_shoulder_vertical_vec = left_shoulder_vertical_vec / np.linalg.norm(left_shoulder_vertical_vec)
+            left_shoulder_angle_pitch = math.acos(left_torso_vertical_vec.dot(left_shoulder_vertical_vec)) - math.pi/2
+        
+        if left_shoulder_angle_roll >= 1.2:
+        left_shoulder_angle_pitch = 0
+        rospy.loginfo('left shoulder pitch 0')
 
-	    # right shoulder roll/pitch
-	    right_shoulder_elbow = right_elbow - right_shoulder
+            #left_shoulder_angle_pitch = -1.0 * math.asin(left_shoulder_elbow[2])
+
+        # right shoulder roll/pitch
+        right_shoulder_elbow = right_elbow - right_shoulder
             right_shoulder_neck = left_shoulder - right_shoulder
 
             right_shoulder_elbow = right_shoulder_elbow / np.linalg.norm(right_shoulder_elbow)
             right_shoulder_neck = right_shoulder_neck / np.linalg.norm(right_shoulder_neck)
 
             right_shoulder_angle_roll = -1.0 * math.acos(right_shoulder_elbow.dot(right_shoulder_neck)) + math.pi/2
+        
+        # neck to torso vector
+            #neck_torso_vec = torso - neck
+            right_torso_vertical_vec = np.cross(neck_torso_vec, right_shoulder_neck)
+            right_torso_vertical_vec = right_torso_vertical_vec / np.linalg.norm(right_torso_vertical_vec)
+            right_shoulder_vertical_vec = np.cross(right_shoulder_neck, right_shoulder_elbow)
+        
+            right_shoulder_vertical_vec = right_shoulder_vertical_vec / np.linalg.norm(right_shoulder_vertical_vec)
+            right_shoulder_angle_pitch = math.acos(right_torso_vertical_vec.dot(right_shoulder_vertical_vec)) - math.pi/2
 
-            right_shoulder_angle_pitch = -1.0 * math.asin(right_shoulder_elbow[2])
+        if right_shoulder_angle_roll >= 1.2:
+        right_shoulder_angle_pitch = 0
+        rospy.loginfo('left shoulder pitch 0')
+        
+        
 
-	    # left elbow roll
- 	    left_elbow_hand = left_hand - left_elbow
+            #right_shoulder_angle_pitch = -1.0 * math.asin(right_shoulder_elbow[2])
+
+        # left elbow roll
+        left_elbow_hand = left_hand - left_elbow
             left_elbow_shoulder = left_shoulder - left_elbow
             left_elbow_hand = left_elbow_hand / np.linalg.norm(left_elbow_hand)
             left_elbow_shoulder = left_elbow_shoulder / np.linalg.norm(left_elbow_shoulder)
             left_elbow_angle_roll =  math.acos(left_elbow_shoulder.dot(left_elbow_hand)) - math.pi
-	    
-	    # right elbow roll
+        
+        # right elbow roll
             right_elbow_hand = right_hand - right_elbow
             right_elbow_shoulder = right_shoulder - right_elbow
             right_elbow_hand = right_elbow_hand / np.linalg.norm(right_elbow_hand)
             right_elbow_shoulder = right_elbow_shoulder / np.linalg.norm(right_elbow_shoulder)
             right_elbow_angle_roll = -1.0 * math.acos(right_elbow_hand.dot(right_elbow_shoulder)) + math.pi
 
-	    # right elbow yaw
-	    right_elbow_angle_yaw = right_elbow_hand[2] / math.sin(right_elbow_angle_roll) * math.pi/2
-            if right_elbow_angle_yaw > 1.5:
-                right_elbow_angle_yaw = 1.5
-            elif right_elbow_angle_yaw < -1.5:
-                right_elbow_angle_yaw = -1.5
+        # right elbow yaw
+        right_elbow_vertical_vec = np.cross(right_shoulder_elbow, right_elbow_hand)
+        right_elbow_vertical_vec = right_elbow_vertical_vec / np.linalg.norm(right_elbow_vertical_vec)
+        right_elbow_angle_yaw = math.acos(right_elbow_vertical_vec.dot(right_shoulder_vertical_vec))
+        tmp = np.cross(right_elbow_vertical_vec, right_shoulder_vertical_vec)
+        right_elbow_angle_yaw -= math.pi
 
-	    # left elbow roll
-            left_elbow_angle_yaw = left_elbow_hand[2] / math.sin(left_elbow_angle_roll) * math.pi/2
-            if left_elbow_angle_yaw > 1.5:
-                left_elbow_angle_yaw = 1.5
-            elif left_elbow_angle_yaw < -1.5:
-                left_elbow_angle_yaw = -1.5
-	    
-    	    # head yaw
-	    head_neck = head - neck
-	    head_neck = head_neck / np.linalg.norm(head_neck)
-	    head_yaw = math.pi/2 - math.asin(head_neck[2])
+        if tmp.dot(right_shoulder_elbow) < 0:
+            right_elbow_angle_yaw = right_elbow_angle_yaw * -1.0
+        
+        left_elbow_vertical_vec = np.cross(left_shoulder_elbow, left_elbow_hand)
+        left_elbow_vertical_vec = left_elbow_vertical_vec / np.linalg.norm(left_elbow_vertical_vec)
+        left_elbow_angle_yaw = math.acos(left_elbow_vertical_vec.dot(left_shoulder_vertical_vec))
+        tmp = np.cross(left_elbow_vertical_vec, left_shoulder_vertical_vec)
+        
+            left_elbow_angle_yaw -= math.pi
 
-	    jdl = jointdatalist()
+        if tmp.dot(left_shoulder_elbow) < 0:
+            left_elbow_angle_yaw = left_elbow_angle_yaw * -1.0
+
+            # head yaw
+        head_neck = head - neck
+        head_neck = head_neck / np.linalg.norm(head_neck)
+        head_yaw = math.pi/2 - math.asin(head_neck[2])
+
+        jdl = jointdatalist()
             jd_pitch = jointdata(Part=String("LShoulderPitch"), Angles=Float32(left_shoulder_angle_pitch))
-	    jd_roll = jointdata(Part=String("LShoulderRoll"), Angles=Float32(left_shoulder_angle_roll))
+        jd_roll = jointdata(Part=String("LShoulderRoll"), Angles=Float32(left_shoulder_angle_roll))
             jdl.joints.append(jd_pitch)
-	    jdl.joints.append(jd_roll)
+        jdl.joints.append(jd_roll)
 
-	    jd_pitch = jointdata(Part=String("RShoulderPitch"), Angles=Float32(right_shoulder_angle_pitch))
-	    jd_roll = jointdata(Part=String("RShoulderRoll"), Angles=Float32(right_shoulder_angle_roll))
+        jd_pitch = jointdata(Part=String("RShoulderPitch"), Angles=Float32(right_shoulder_angle_pitch))
+        jd_roll = jointdata(Part=String("RShoulderRoll"), Angles=Float32(right_shoulder_angle_roll))
             jdl.joints.append(jd_pitch)
-	    jdl.joints.append(jd_roll)
+        jdl.joints.append(jd_roll)
 
-	    jd_roll = jointdata(Part=String("LElbowRoll"), Angles=Float32(left_elbow_angle_roll))
-	    jd_yaw = jointdata(Part=String("LElbowYaw"), Angles=Float32(left_elbow_angle_yaw))
-	    jdl.joints.append(jd_roll)
-	    jdl.joints.append(jd_yaw)
+        jd_roll = jointdata(Part=String("LElbowRoll"), Angles=Float32(left_elbow_angle_roll))
+        jd_yaw = jointdata(Part=String("LElbowYaw"), Angles=Float32(left_elbow_angle_yaw))
+        jdl.joints.append(jd_roll)
+        jdl.joints.append(jd_yaw)
 
-	    jd_roll = jointdata(Part=String("RElbowRoll"), Angles=Float32(right_elbow_angle_roll))
-	    jd_yaw = jointdata(Part=String("RElbowYaw"), Angles=Float32(right_elbow_angle_yaw))
-	    jdl.joints.append(jd_roll)
-	    jdl.joints.append(jd_yaw)
+        jd_roll = jointdata(Part=String("RElbowRoll"), Angles=Float32(right_elbow_angle_roll))
+        jd_yaw = jointdata(Part=String("RElbowYaw"), Angles=Float32(right_elbow_angle_yaw))
+        jdl.joints.append(jd_roll)
+        jdl.joints.append(jd_yaw)
 
-	    jd_yaw = jointdata(Part=String("HeadPitch"), Angles=Float32(head_yaw))
-	    # jd_roll = jointdata(Part=String("HeadPitch"), Angles=Float32(head_pitch))
+        jd_yaw = jointdata(Part=String("HeadPitch"), Angles=Float32(head_yaw))
+        # jd_roll = jointdata(Part=String("HeadPitch"), Angles=Float32(head_pitch))
 
             jdl.joints.append(jd_yaw)
-	    # jdl.joints.append(jd_roll)
+        # jdl.joints.append(jd_roll)
 
             self.pub_trans.publish(jdl)
-            rospy.loginfo('pitch:{0}'.format(head_yaw/math.pi*180))
             r.sleep()
 
 if __name__ == '__main__':
